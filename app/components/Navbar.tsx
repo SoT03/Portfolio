27e379ce2d5e7e211c0e1be19ee6712d3822fcb1
SoT03 <bogdan.zygadlo123@gmail.com>
2024-03-23 15:56:54 +0100

    recolored header

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar() {
 	const [isNavOpen, setIsNavOpen] = useState(false);
 
 	return (
-		<nav className='fixed top-0 flex justify-end w-full   bg-transparent   xl:w-1/2 lg:justify-center  z-50'>
+		<nav className='fixed top-0 flex justify-end w-full   bg-transparent   xl:w-1/2 lg:justify-center   z-50'>
 			<div
 				className='relative flex flex-col z-30 gap-1.5 p-4 mt-2 mr-4 self-end cursor-pointer md:p-10 md:gap-2 lg:hidden '
 				onClick={() => setIsNavOpen(!isNavOpen)}>
@@ -44,13 +44,13 @@ export default function Navbar() {
 			</div>
 
 			<ul
-				className={`absolute  h-screen w-screen top-0 left-0 flex flex-col items-center justify-center  bg-black transition-all duration-700 z-20  lg:opacity-100 lg:-translate-y-0 lg:relative lg:h-auto lg:flex-row     ${
+				className={`absolute  h-screen w-screen top-0 left-0 flex flex-col items-center justify-center  bg-black transition-all duration-700 z-20 lg:bg-slate-700     lg:opacity-100 lg:-translate-y-0 lg:relative lg:h-auto lg:flex-row lg:shadow-lg  xl:top-12   xl:rounded-e-full ${
 					isNavOpen ? 'opacity-100' : 'opacity-0 -translate-y-full'
 				} `}>
 				{navItems.map((item) => (
 					<li
 						key={item.text}
-						className='text-2xl text-white my-2 text-center w-1/2 border-b-2 md:my-6 md:text-3xl md:pb-2 md:w-1/4 lg:border-none lg:pb-0 lg:text-2xl  '>
+						className='text-2xl text-white my-2 text-center w-1/2 border-b-2 md:my-6 md:text-3xl md:pb-2 md:w-1/4 lg:border-none lg:pb-0 lg:text-lg  lg:my-4'>
 						<Link href={item.link} className='py-2 px-4 lg:relative link'>
 							{item.text}
 						</Link>
