1fbab04595e4d6663ea4f87aa105a58036f66541
SoT03 <bogdan.zygadlo123@gmail.com>
2024-06-13 14:33:20 +0200

    changed project section

--- app/components/Project/ProjectCard.tsx
+++ app/components/Project/ProjectCard.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 
 import Link from 'next/link';
+import { Github, Radio, WifiIcon } from 'lucide-react';
 
 type ProjectCardProps = {
 	title: string;
@@ -14,52 +15,49 @@ type ProjectCardProps = {
 
 export default function ProjectCard({
 	title,
-	desc,
 	tech,
 	live,
 	repo,
 	img,
 }: ProjectCardProps) {
 	return (
-		<div className='flex flex-col shadow-md rounded-lg overflow-hidden bg-white'>
-			<div className=''>
-				<img
-					loading='lazy'
-					src={img}
-					alt={`${title} website`}
-					height={900}
-					width={1600}
-				/>
-			</div>
-
-			<div className='flex flex-col justify-between  py-4 px-6 h-[300px] md:px-12 lg:h-[260px] '>
-				<div>
-					<h4 className='mb-2 font-bold text-xl md:text-2xl md:my-3'>
-						{title}
-					</h4>
-					<p className='mb-2 text-sm md:text-base'>{desc}</p>
+		<div className='mb-14 bg-neutral-100 rounded-lg'>
+			<div>
+				<div className='relative p-3 '>
+					<img
+						alt={`${title} project screenshot`}
+						className='rounded-lg '
+						src={img}
+					/>
 				</div>
-				<div className=' flex justify-between py-3 md:py-4 '>
-					<div className=''>
+			</div>
+			<div className='p-2 '>
+				<div className='flex flex-col justify-center items-center md:py-2'>
+					<div className='text-lg font-semibold mb-2 md:mb-4 md:text-2xl'>
+						{title}
+					</div>
+					<div className='  '>
 						<img
 							loading='lazy'
-							width={32}
-							height={32}
 							src={`https://skillicons.dev/icons?i=${tech}`}
 							alt={`icon`}
-							className='w-20 md:w-24 xl:w-28'
+							className='relative '
 						/>
 					</div>
-					<div className='relative flex items-center gap-2 md:gap-6 md:text-xl lg:mr-6 '>
+				</div>
+				<div className='py-4 '>
+					<div className=' relative flex items-center justify-around   md:text-xl md:py-4 '>
 						<Link
+							target='_blank'
 							href={live}
-							className=' hover:text-blue-500 transition-colors duration-300 '>
-							Live
+							className='flex gap-2 items-center justify-center border py-2 px-4 md:w-1/4 hover:text-blue-500 transition-colors duration-300 '>
+							Live <Radio />
 						</Link>
 						<Link
+							target='_blank'
 							href={repo}
-							className='hover:text-blue-500 transition-colors duration-300'>
-							Repository
+							className='flex gap-2 items-center justify-center border py-2 px-4 md:w-1/4 hover:text-blue-500 transition-colors duration-300'>
+							Repository <Github />
 						</Link>
 					</div>
 				</div>
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import Section from './section/Section';
 import SectionTitle from './section/SectionTitle';
 import SectionSubTitle from './section/SectionSubTitle';
@@ -8,6 +8,8 @@ import cryptoImg from '../assets/CryptoCoin.jpg';
 import tqmImg from '../assets/tqm.jpg';
 import gymbroImg from '../assets/GymBro.jpg';
 import darwinImg from '../assets/Darwin.jpg';
+import dashboard from '../assets/dashboard.png';
+import shop from '../assets/shop.png';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -16,38 +18,53 @@ import { Pagination } from 'swiper/modules';
 const projectData = [
 	{
 		id: 1,
+		title: 'Store Admin Dashboard',
+		desc: '',
+		tech: 'nextjs,prisma,tailwind,supabase',
+		live: 'https://e-shop-admin-lac.vercel.app',
+		repo: 'https://github.com/SoT03/e-shoes',
+		img: dashboard.src,
+	},
+	{
+		id: 2,
+		title: 'Store Client',
+		desc: '',
+		tech: 'nextjs,prisma,supabase',
+		live: 'https://e-shop-client-mocha.vercel.app/',
+		repo: 'https://github.com/SoT03/e-shoes',
+		img: shop.src,
+	},
+	{
+		id: 3,
 		title: 'CryptoCoin',
-		desc: 'CryptoCoin represents my initial foray into web development, focusing on API integration—a fundamental skill in modern web development. Through this project, I gained hands-on experience in fetching and displaying real-time cryptocurrency data.',
+		desc: '',
 		tech: 'nextjs,react,sass',
 		live: 'https://crypto-calc-two.vercel.app/',
 		repo: 'https://github.com/SoT03/CryptoCalc',
 		img: cryptoImg.src,
 	},
 	{
-		id: 2,
+		id: 4,
 		title: 'GymBro',
-		desc: 'GymBro marked my inaugural venture into utilizing Tailwind CSS. My objective was clear: crafting a sleek and minimalist webpage that exuded both simplicity and visual appeal.',
+		desc: '',
 		tech: 'nextjs,react,tailwind',
 		live: 'https://gymbro-silk.vercel.app/',
 		repo: 'https://github.com/SoT03/gymbro',
 		img: gymbroImg.src,
 	},
-];
-
-const commercialProjects = [
 	{
-		id: 1,
+		id: 5,
 		title: 'TQM Partners',
-		desc: 'The company TQM Partners is a firm providing training, implementation, and consultancy services in the scope of ISO, for which I have developed the website.',
+		desc: '',
 		tech: 'nextjs,react,tailwind',
 		live: 'TQMpartners.pl',
 		repo: 'https://github.com/SoT03/tqm',
 		img: tqmImg.src,
 	},
 	{
-		id: 2,
+		id: 6,
 		title: 'Darwin FHU',
-		desc: 'Darwin FHU is a company providing services in the field of photovoltaic installations, heat pumps, air conditioning, and energy storage',
+		desc: '',
 		tech: 'nextjs,react,tailwind',
 		live: 'dar-win.pl/',
 		repo: 'https://github.com/SoT03/darwin',
@@ -56,40 +73,21 @@ const commercialProjects = [
 ];
 
 export default function Projects() {
+	const [numberOfSlides, setNumberOfSlides] = useState(1);
+
 	return (
-		<Section id='projects' classes='relative bg-slate-200'>
+		<Section id='projects' classes='relative bg-slate-200 '>
 			<SectionTitle text='Projects' />
 			<SectionSubTitle text='My personal and commercial projects' />
-			<div className='wrapper '>
-				<div className='my-10 lg:my-6'>
-					<h3 className='text-gray-700 mb-6 text-2xl font-semibold'>
-						Personal Projects
-					</h3>
+			<div className='wrapper'>
+				<div className='my-12  lg:my-12'>
 					<Swiper
 						modules={[Pagination]}
-						spaceBetween={50}
-						slidesPerView={1}
+						spaceBetween={40}
+						slidesPerView={numberOfSlides}
 						scrollbar={{ draggable: true }}
 						pagination={{ type: 'bullets', clickable: true }}>
 						{projectData.map((project) => (
-							<SwiperSlide key={project.id}>
-								<ProjectCard key={project.id} {...project} />
-							</SwiperSlide>
-						))}
-					</Swiper>
-				</div>
-				<div>
-					<h3 className='text-gray-700 mb-6 text-2xl font-semibold'>
-						Commercial Projects
-					</h3>
-					<Swiper
-						className='flex'
-						modules={[Pagination]}
-						spaceBetween={50}
-						slidesPerView={1}
-						scrollbar={{ draggable: true }}
-						pagination={{ type: 'bullets', clickable: true }}>
-						{commercialProjects.map((project) => (
 							<SwiperSlide key={project.id}>
 								<ProjectCard key={project.id} {...project} />
 							</SwiperSlide>
